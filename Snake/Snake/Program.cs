bc535da7ffed7c4b300b093f95738a5a4337498d
Stas Kuznetsov <stas85k@gmail.com>
2016-02-13 09:37:33 +0200

    Update Program.cs

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,7 +13,7 @@ namespace Snake
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new  Point(4, 5, '#');
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
             Console.ReadLine();
