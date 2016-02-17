6432786bca43489b8cbc125a32f26501a4342ff2
Stas Kuznetsov <stas85k@gmail.com>
2016-02-17 13:52:05 +0200

    Drawing a line

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,26 +16,9 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.Remove(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
+            HorizontalLine line = new HorizontalLine();
+            line.Drow();
+            
             Console.ReadLine();
         }
     }
