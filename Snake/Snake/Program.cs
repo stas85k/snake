a84d0f7eec609049c78ddd570dcd94404c7b5fe7
Stas Kuznetsov <stas85k@gmail.com>
2016-02-17 18:29:15 +0200

    The Figure Class is created.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,22 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(120, 30);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //The frame is drawn
+            HorizontalLine upLine = new HorizontalLine(0, 118, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 118, 29, '+');
+            VerticalLine leftLine = new VerticalLine(0, 29, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 29, 118, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            HorizontalLine lineH1 = new HorizontalLine(5, 20, 8, '+');
-            lineH1.Drow();
-            HorizontalLine lineH2 = new HorizontalLine(5, 20, 16, '+');
-            lineH2.Drow();
-            VerticalLine lineV1 = new VerticalLine(8, 16, 5, '+');
-            lineV1.Drow();
-            VerticalLine lineV2 = new VerticalLine(8, 16, 20, '+');
-            lineV2.Drow();
 
-            Console.ReadLine();
+
+            //The points are drawn
+            Point p = new Point(4, 5, '*');
+            p.Draw();
         }
     }
 }
--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Snake
+namespace Snake 
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pList;
-
         public VerticalLine(int yTop, int yBottom, int x, char sym)
         {
             pList = new List<Point>();
@@ -17,14 +15,6 @@ namespace Snake
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
-            }
-        }
-
-        public void Drow()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
             }
         }
     }
