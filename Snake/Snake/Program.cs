7e34d72966d8ea4bdb127b56486c3ceef0435c37
Stas Kuznetsov <stas85k@gmail.com>
2016-02-19 13:33:41 +0200

    Hit processing.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,6 +11,71 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(120, 30);
+            Walls walls = new Walls(120, 30);
+            walls.Draw();
+            
+            //The points are drawn
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(128, 28, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandlKey(key.Key);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
             Draw(v1);
 
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -33,6 +33,17 @@ namespace Snake
             head.Draw();
         }
 
+        internal bool IsHitTail()
+        {
+            var head = pList.Last();
+            for(int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                    return true;
+            }
+            return false;
+        }
+
         private Point GetNextPoint()
         {
             Point head = pList.Last();
